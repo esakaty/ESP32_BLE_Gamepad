4db2474c8857cf21ac347a7e5d085b7f7dbde689
imomochi <lion.toaster@gmail.com>
2018-12-23 21:18:57 +0900

    空のプロジェクト

---
+++ ESP32_BLE_Gamepad/ESP32_BLE_Gamepad.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ESP32_BLE_Gamepad.ino
+ Created:	2018/12/23 21:18:13
+ Author:	esakat
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
